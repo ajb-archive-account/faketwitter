c6eb479cedc59a0f31ac4b2325734b271c4d4f03
andybyers21 <andy.byers21@gmail.com>
2020-09-21 17:22:05 +0100

    added the tweet form

--- faketwitter/tweets/views.py
+++ faketwitter/tweets/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
+from .forms import TweetForm
 from .models import Tweet
 
 
@@ -11,6 +12,16 @@ def home_view(request, *args, **kwargs):
     """Set the homepage view"""
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # Do other form related logic here.
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
 
 
 def tweet_list_view(request, *args, **kwargs):
