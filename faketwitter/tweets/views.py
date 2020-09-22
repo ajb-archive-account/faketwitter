d05b91850cba9d4eb4b7b64264886ebe958d0b17
andybyers21 <andy.byers21@gmail.com>
2020-09-22 10:26:26 +0100

    set server response

--- faketwitter/tweets/views.py
+++ faketwitter/tweets/views.py
@@ -21,11 +21,14 @@ def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
     # print('post data is', request.POST)
     next_url = request.POST.get('next') or None
-    print('next url is', next_url)
+
     if form.is_valid():
         obj = form.save(commit=False)
         # Do other form related logic here.
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
+
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
