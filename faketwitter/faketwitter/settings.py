e76c9c5c92ed4ee2ca8025910a6155bbba9a80fa
andybyers21 <andy.byers21@gmail.com>
2020-09-23 13:25:51 +0100

    install django rest framework

--- faketwitter/faketwitter/settings.py
+++ faketwitter/faketwitter/settings.py
@@ -67,7 +67,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # Third-Party Authentication
+    'rest_framework',
+    # Internal
     'tweets',
 ]
 
